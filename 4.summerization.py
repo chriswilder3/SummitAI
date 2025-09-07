596776ae87016603a285949656ae6337b25bb6a3
chriswilder3 <chriswilderdelta19@gmail.com>
2025-09-07 18:11:55 +0530

    advanced scalable map reduce summerization

--- 4.summerization.py
+++ 4.summerization.py
@@ -10,6 +10,9 @@ llm = ChatOpenAI(
 )
 
 from langchain_core.prompts import PromptTemplate
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.docstore.document import Document
+from langchain.chains.summarize import load_summarize_chain
 
 def summarize(llm, transcript):
     print("---Summerizing---")
@@ -28,6 +31,62 @@ def summarize(llm, transcript):
     response = chain.invoke({"transcript":transcript})
     return response.content
 
+
+def map_reduce_summarize(llm, transcript_segments):
+
+    
+    # Step 1: Flatten transcript into grouped chunks
+    transcript_text = " ".join([f"{seg['speaker']}: {seg['text']}"
+                                 for seg in transcript_segments])
+    
+    # Step 2: Split into Documents (chunks of ~2000 chars)
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size = 2000,
+        chunk_overlap = 200,
+        separators = ["\n\n", "\n", ".", " "]
+    )
+    docs = [ Document(page_content=chunk)  
+                for chunk in text_splitter.split_text(transcript_text)
+            ]
+    
+    map_prompt = """
+        You are a meeting assistant. Summarize the following transcript chunk.
+
+        Transcript:
+        {text}
+
+        Your summary should include:
+        - Main discussion points
+        - Speaker perspectives
+        - Any implied disagreements or concerns
+
+        Write at least 3 bullet points.
+    """
+
+    reduce_prompt = """
+        Combine the following partial summaries into a final meeting summary.
+
+        Partial summaries:
+        {text}
+
+        Write:
+        1. A detailed summary.
+        2. Key decisions made.
+        3. Action items (with responsible person if available).
+    """
+    # Step 3: Load map-reduce summarization chain
+    chain = load_summarize_chain(
+            llm, chain_type="map_reduce",
+            map_prompt= PromptTemplate(template=map_prompt, 
+                                       input_variables=["text"]),
+            combine_prompt= PromptTemplate(template=reduce_prompt,
+                                        input_variables=["text"]),
+            verbose=True)
+    
+    # Step 4.
+    response = chain.invoke(docs)
+    return response
+
 def main():
     input_audio_file = "E:/downloads/meeting_preprocessed.wav"
     
@@ -38,10 +97,11 @@ def main():
     transcript_text = " ".join([seg["text"] for seg in transcript_segments])
     
     # Step 2: Summarize
-    summary = summarize(llm, transcript_text)
-    
+    # summary = summarize(llm, transcript_text)
+    summary = map_reduce_summarize(llm, transcript_segments)
+
     print("📌 Meeting Summary:\n")
-    print(summary)
+    print(summary['output_text'])
 
 if __name__ == "__main__":
     main()
